3d2bc068ac06cbc8a919c724823c824f03bae83e
r-santtos <rsanttos.dev@gmail.com>
2023-04-21 15:10:11 -0300

    criando class SignIn

---
+++ class/SignIn.php
@@ -0,0 +1,43 @@
+<?php     
+  class SignIn {
+    // Mock Banco de Dados
+    private $mock_email = "rsanttos.dev@gmail.com";
+    private $mock_password = "123456";
+      
+    // Atributos da classe Sign In
+    private $email;
+    private $pass;
+
+    //--------------------------------------------------------
+    // GET & SET DE EMAIL
+    public function getEmail() {
+      return $this->email;
+    }
+    public function setEmail(string $parameter) {
+      $this->email = $parameter;
+    }
+
+    //--------------------------------------------------------
+    // GET & SET DE PASS
+    public function getPass() {
+      return $this->pass;
+    }
+    public function setPass(string $parameter) {
+      $this->pass = $parameter;
+    }
+
+    //--------------------------------------------------------
+    // Método da classe SignIn
+    public function Login() {
+      if($this->email == $this->mock_email and $this->pass == $this->mock_password) {
+        echo "Login successful!";
+      } else {
+        echo "Login error";
+      }
+    }
+  }
+
+  $signin = new SignIn();
+  $signin-> setEmail("rsanttos.dev@gmail.com");
+  $signin-> setPass("123456");
+  $signin->Login();
